e77e7edab9509447c3feebc5ffc3078f01e03d66
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2024-03-30 14:07:18 +0100

    Update pjointes.sh

--- src/scripts/pjointes.sh
+++ src/scripts/pjointes.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+#enregistrer les pièces jointes d'un message Thunderbird
+#nécessite cd'avoir installé espeak et xdotool
+
+amixer -q  -D pulse sset Master toggle # on coupe le haut-parleur mais on garde espeak
+sleep 1
+xdotool key Alt+m
+sleep 1
+xdotool key j
+sleep 1
+xdotool key t
+sleep 1
+xdotool key Alt+Home
+sleep 1
+amixer -q  -D pulse sset Master toggle # on rétablit le haut-parleur
+espeak -a 200 -v mb-fr1 -s 150 "descendez sur un dossier. Et validez deux fois"
+
+exit 0;
 
