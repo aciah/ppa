f9ff64a9635137166baed76f4a6d68cfc2328d6b
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2024-03-30 16:22:58 +0100

    script facilitant l'enregistrement d'un document LibreOffice Writer
    le script crée le dossier A-attente, 
    le document s'enregistre sous le nom a-classer.

--- src/scripts/save4.sh
+++ src/scripts/save4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script facilitant l'enregistrement d'un document LibreOffice Writer.
-# le script crée le dossier A-attente
+# le script crée le dossier A-attente.
 # le document s'enregistre sous le nom a-classer.
 # on peut ensuite reprendre ce document pour modifier son nom et le déplacer.
 
