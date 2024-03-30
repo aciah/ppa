a3effd1d29cfe3558b77e3ee286325ff031193a5
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2024-03-30 20:54:10 +0100

    script pur décompresser une archive

--- src/scripts/dezip.sh
+++ src/scripts/dezip.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
+# script pour décompresser une archive
+# on peut régler le clavier pour lancer ce script avec la touche F10.
+
+aplay /usr/local/share/advl/beep.wav
+
+File="$1"
+Y="$(pwd)"
+mkdir $HOME/Dossier-dezip
+cp "$File" /home/aciah/Dossier-dezip       # si aciah est le nom du dossier personnel. Sinon remplace aciah par le user.
+
+ZIPDIR=$HOME/Dossier-dezip
+TMP=/tmp/zipfiles
+cd "$ZIPDIR"
+rm tmp/zipfiles 2>/dev/null
+
+for i in ./*.zip; do
+    dossier=${i::-4}
+    mkdir "$dossier"
+    unzip -o "$i" -d "$dossier" 
+    subdirs=$(find "$dossier" -type d -printf ".\n" | wc -l)
+    if [[ $subdirs -gt 1 ]]; then
+        mv ./$dossier/*/* ./$dossier/*
+    
+#    rm /home/aciah/Dossier-dezip/*.zip
+
+    fi
+done
+
+sleep 5
+espeak -a 300 -v mb-fr1 -s 150 "le document ob tenu se trouve dans le Dossier-dezip"
+
+rm /home/aciah/Dossier-dezip/*.zip
+
+caja  /home/aciah/Dossier-dezip
+
 
