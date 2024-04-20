a103af5d650fcdfde82b98f5ffbd815dd0805b10
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2024-04-20 21:09:35 +0200

    Mise à jour du script A-lire-et-obtenir-le-texte.sh

--- src/scripts/A-lire-et-obtenir-le-texte.sh
+++ src/scripts/A-lire-et-obtenir-le-texte.sh
@@ -1,92 +1,72 @@
 # ﻿#!/bin/sh
-# VERSION 1.0
-# AUTEUR  Jean-Yves ROCHER
-# Modifications : ACIAH et Pierre Estrem, décembre 2022
-# NAME : Lire et obtenir le texte
-# DESCRIPTION : Script permettant la lecture en direct de fichiers images jpg, png, tif, pdf, doc, docx et html. Le script donne aussi le texte en .txt dans le dossier d'origine.
-# DEPENDANCES : le script nécessite les paquets pdftoppm, tesseract, pdftotext, unoconv.
-# il faut créer le répertoire Machine-a-lire dans $HOME .
-# COMPLEMENTS : le script fait appel à des fichiers sonores placés dans le dossier : /usr/local/share/advl :  frapper.pdf, patientez.pdf, FrappezF5.pdf, terminer.pdf.
-# Le script fait aussi appel au script conv-txt.sh qui se trouve dans /home/aciah/.config/caja/scripts qui est appelé par le raccourci F5.
-# RACCOURCIS : le script A-lire-et-obtenir-le-texte se trouve dans /home/aciah/.config/caja/scripts et est appelé par le raccourci F8. Les deux raccourcis sont à définir dans /home/aciah/.config/caja/accels
-# LECTURE : la lecture se fait avec le script lecture.sh qui se trouve dans /usr/bin/. Avec la flèche-bas on diminue le rythme de lecture. Avec la flèche-haut on augmente le rythme de lecture.
-
+# VERSION 2.0
+# AUTHOR  Jean-Yves ROCHER
+# Modifications : ACIAH et Pierre Estrem, avril 2024
+# NAME : Lecture à la volée
+# DESCRIPTION : Script permettant la lecture endirect de fichiers images jpg, png, tif, pdf, doc, docx.
+# Dépendances : espeak, unoconv, pandoc, player.py etnotre script conv-txt.sh
 # Décommenter les deux lignes suivantes pour récupérer les log du script et les afficher en direct dans un terminal
-# exec 1>>/var/log/aciah/lireALaVolee.log 2>>/var/log/aciah/lireALaVolee.log
-# xterm -e "tail -f /var/log/aciah/lireALaVolee.log" &
+#exec 1>>/var/log/aciah/lireALaVolee.log 2>>/var/log/aciah/lireALaVolee.log
+#xterm -e "tail -f /var/log/aciah/lireALaVolee.log" &
 
  FICHIER=`basename $1`
  FILE=`basename ${FICHIER%.*}`
  CHEMIN=`dirname $1`
  REPLECTURE="$HOME"/Machine-a-lire
  TYPE=`mimetype -i -b $1`
-aplay /usr/local/share/advl/beep.wav  # petit bruit
 
-pdftotext -layout /usr/local/share/advl/frappez.pdf - | espeak -a 200 -v mb-fr1 -s 130  #rappelle qu'il faut frapper la touche Q à la fin de la lecture.
-# on coupe la voix Orca, il ne faudra pas oublier de la relancer.
-if pgrep "orca" > /dev/null; then
-        killall orca &
-        sleep 2
-        killall -9 speech-dispatcher &
+aplay /usr/local/share/advl/beep.wav
+
+######## Fichier de type doc  ############
+
+if [ ! -z "`echo "$TYPE" | grep -i 'doc' `" ]; then
+espeak -a 200 -v mb-fr1 -s 150 "patien tez"
 fi
+$HOME/.config/caja/scripts/conv-txt.sh "$1"
 
-######### Fichier de type PDF   ######### 
+
+######### Fichier de type PDF   #########
+mkdir $HOME/Machine-a-lire
  if [ ! -z "`echo "$TYPE" | grep -i 'pdf' `" ]; then
          pdftoppm -r 200 -tiff "$CHEMIN"/"$FICHIER" "$REPLECTURE"/alavolee
              for alavolee in "$REPLECTURE"/*.tif; do
-tesseract "$alavolee" repf
-
-x-terminal-emulator -e /usr/bin/lecture.sh repf.txt
+tesseract "$alavolee" volee
+x-terminal-emulator -e /usr/local/bin/player.py volee.txt
              done
-mv repf.txt ${FICHIER%\.*}.txt
 
- fi
- 
-########  Fichier de type PNG  #####
+mv volee.* ${1%\.*}.txt
+rm $HOME/Machine-a-lire/alavolee*
+fi
+########  Fichier de type PNG ou JPG   #####
   if [ ! -z "`echo "$TYPE" | grep -i -e 'png' `" ]; then echo  
-tesseract "$CHEMIN"/"$FICHIER" repp
-x-terminal-emulator -e /usr/bin/lecture.sh repp.txt
+tesseract "$CHEMIN"/"$FICHIER" resul
+x-terminal-emulator -e /usr/local/bin/player.py resul.txt
+mv resul.txt ${1%\.*}.txt
   fi
-mv repp.txt ${FICHIER%\.*}.txt
-
-########  Fichier de type jpg  ##### 
 
   if [ ! -z "`echo "$TYPE" | grep -i -e 'jpeg' `" ]; then echo  
-tesseract "$CHEMIN"/"$FICHIER" repj
-x-terminal-emulator -e /usr/bin/lecture.sh repj.txt
+tesseract "$CHEMIN"/"$FICHIER" result
+x-terminal-emulator -e /usr/local/bin/player.py result.txt
+mv result.txt ${1%\.*}.txt
   fi
-mv repj.txt ${FICHIER%\.*}.txt
 
 
-rm $HOME/Machine-a-lire/alavolee*
-rm repf.*
-rm repp.*
-rm repj.*
-
-########### Fichier odt, doc, html #############
-
- if [ ! -z "`echo "$TYPE" | grep -i 'doc' `" ]; then
-pdftotext -layout /usr/local/share/advl/patientez.pdf - | espeak -a 200 -v mb-fr1 -s 150
-fi
-File="$1"
-unoconv --doctype=document --format=txt "$File"
-cp "${File%\.*}.txt" $HOME/Machine-a-lire/
-x-terminal-emulator -e /usr/bin/lecture.sh $HOME/Machine-a-lire/"${File%\.*}.txt"
-fi
-
-if (mimetype==html); then
-pdftotext -layout /usr/local/share/advl/FrappezF5.pdf - | espeak -a 200 -v mb-fr1 -s 150
-fi
-
-if [ ! -z "`echo "$TYPE" | grep -i 'odt' `" ]; then
-pdftotext -layout /usr/local/share/advl/FrappezF5.pdf - | espeak -a 200 -v mb-fr1 -s 150
-fi
+######## Fichier de type docx  ############
 
 if [ ! -z "`echo "$TYPE" | grep -i 'docx' `" ]; then
-#pdftotext -layout /usr/local/share/advl/patientez.pdf - | espeak -a 200 -v mb-fr1 -s 150
+espeak -a 200 -v mb-fr1 -s 150 "patien tez"
 fi
+pandoc "$CHEMIN"/"$FICHIER"
 
+espeak -a 200 -v mb-fr1 -s 130 "lecture termi née"
 
+######## Fichier de type odt  ############
 
-#pdftotext -layout /usr/local/share/advl/terminer.pdf - | espeak -a 200 -v mb-fr1 -s 150
-exit
+if [ ! -z "`echo "$TYPE" | grep -i 'odt' `" ]; then
+espeak -a 200 -v mb-fr1 -s 150 "patien tez"
+fi
+odt2txt "$CHEMIN"/"$FICHIER"
+
+espeak -a 200 -v mb-fr1 -s 130 "lecture termi née"
+
+exit 0
