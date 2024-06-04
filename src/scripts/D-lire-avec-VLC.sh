f929996a91c4111aca4d2df2301f58515350d50b
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2024-06-04 13:15:55 +0200

    D-lire-avec-VLC.sh

--- src/scripts/D-lire-avec-VLC.sh
+++ src/scripts/D-lire-avec-VLC.sh
@@ -1,1 +1,27 @@
+Ce script permet notamment de lancer la lecture d'un livre sonore, en utilisant VLC
+
+#!/bin/sh
+
+# VERSION 1.0
+# Auteur : Association ACIAH - avril 2023
+# Licence : GPL - v3
+
+# MODIFICATIONS : Association ACIAH, Gérard Ruau 03-06-2024
+#                 Simplifié le script en supprimant l'ouverture d'une fenêtre de Terminal et de variables non utilisées ;
+#                 modifié la commande cvlc en svlc car lors de la fermeture avant la fin de la vidéo,
+#                 le processus continuait de tourner
+# NOM : D-lire-avec-VLC.sh
+# DESCRIPTION : Script permettant de lire avec VLC
+
+# RACCOURCIS : Ce script est placé dans le dossier $HOME/.config/caja/scripts
+# et est appelé par le raccourci : F12
+# 
+
+
+# $1 est le fichier passé en paramètre
+File="$1"
+
+svlc --play-and-exit "$File"
+
+exit 0
 
