29061f22ece7e21cd71e2961627e3305a23387ec
aciah <104137761+aciah@users.noreply.github.com>
2024-07-05 21:57:31 +0200

    dezip.sh version mai 2024

--- src/scripts/dezip.sh
+++ src/scripts/dezip.sh
@@ -1,5 +1,100 @@
 #!/bin/bash
-# script pour décompresser une archive.
+# # VERSION 1.0
+# AUTHOR : association ACIAH
+# Licence : GPL V3
+# Modifications : Gérard Ruau (membre association ACIAH), 12 mai 2024
+# NAME : dezip;sh 
+# DESCRIPTION : script pour décompresser en utilisant une seule touche
+# RACCOURCIS : ce script est placé dans le dossier $HOME/.config/caja/scripts
+# et est appelé par le raccourci : F10 dans Caja et F12 dans Thunar (problème raxxourcis GTK !)
+
+# Décommenter les deux lignes suivantes pour récupérer les log du script et les afficher en direct dans un terminal
+#  exec 1>>/var/log/aciah/dezip.log 2>>/var/log/aciah/dezip.log
+#  xterm -e "tail -f /var/log/aciah/dezip.log" &
+
+aplay /usr/local/share/advl/beep.wav
+
+FILE="$1"
+Y="$(pwd)"
+mkdir $HOME/Dossier-dezip
+
+cp "$FILE" $HOME/Dossier-dezip
+
+ZIPDIR=$HOME/Dossier-dezip
+TMP=/tmp/zipfiles
+cd "$ZIPDIR"
+rm tmp/zipfiles 2>/dev/null
+
+# Détermination de l'extension du fichier
+FILE_PATH="$FILE"
+FILENAME="$(basename $FILE_PATH)"
+EXTENSION="${FILENAME##*.}"
+
+#Extraction des fichiers en fonction des extensions
+    if [ ! -z $EXTENSION ]
+        then
+            case $EXTENSION in
+
+                #####pour les fichiers .zip
+                zip)
+                    for i in *.zip
+                        do
+                            DOSSIER=$(basename $FILE .zip)
+                            mkdir "$DOSSIER"
+                            unzip -o "$i" -d "$DOSSIER"
+                            rm "$ZIPDIR"/*.zip
+                        done
+                ;;
+                
+                #####pour les fichiers .tar.gz
+                gz)
+                    for f in *.tar.gz
+                        do
+                            DOSSIER=$(basename $FILE .gz)
+                            mkdir "$DOSSIER"
+                            tar zxvf "$f" -C  "$ZIPDIR"
+                            rm "$ZIPDIR"/*.tar.gz
+                            rmdir "$ZIPDIR"/*.tar
+                        done
+                ;;
+                    
+                #####pour les fichiers .tar.bz2
+                bz2)
+                   for k in *.tar.bz2
+                        do
+                            DOSSIER=$(basename $FILE .bz2)
+                            mkdir "$DOSSIER"
+                            tar jxvf "$k" -C  "$ZIPDIR"
+                            rm "$ZIPDIR"/*.tar.bz2
+                            rmdir "$ZIPDIR"/*.tar
+                        done
+                ;;
+                
+                #####pour les fichiers non gérés ci-dessus : ouverture du gestionnaire d'archives => fonctionne mieux avec xarchiver
+                *)
+                    xarchiver --extract-to="$ZIPDIR" "$1"
+                    #      file-roller "$1"
+                    rm "$ZIPDIR"/"$FILENAME"
+                ;;
+
+            esac
+    fi
+
+sleep 3
+# on peut modifier la vitesse, le timbre de voix ainsi que le volume de espeak, voir espeak --help dans un terminal
+espeak -a 300 -v mb-fr1 -s 130 "le document ob tenu est dans le Dossier-dezip qui sera ouvert"
+aplay /usr/local/share/advl/beep.wav
+aplay /usr/local/share/advl/beep.wav
+caja  $HOME/Dossier-dezip
+
+
+############################################
+
+
+
+
+#!/bin/bash
+# script pour décompresser une archive. Version obsolète
 # on peut régler le clavier pour lancer ce script avec la touche F10.
 
 aplay /usr/local/share/advl/beep.wav
